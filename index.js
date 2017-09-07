0033a72c597be2d82b6011df94b3a203fd7a6b42
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2017-09-07 10:56:32 +0200

    [fix] add performance.now polyfill.

--- index.js
+++ index.js
@@ -2,5 +2,24 @@
 'use strict';
 
 module.exports = {
-	name: 'ember-task-scheduler'
+	name: 'ember-task-scheduler',
+
+	importPolyfill: function(app) {
+		var polyfill = 'vendor/performance-polyfill.js';
+
+		if (this.import) {  // support for ember-cli >= 2.7
+			this.import(polyfill, { prepend: true });
+		} else if (app.import) { // support ember-cli < 2.7
+			app.import(polyfill, { prepend: true });
+		} else {
+			console.warn('Please run: ember install ember-cli-import-polyfill')
+		}
+	},
+
+	included: function(app) {
+		this._super.included.apply(this, arguments);
+		this.app = app;
+		this.importPolyfill(app);
+	}
+
 };
